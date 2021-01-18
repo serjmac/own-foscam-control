a5601b997f22429362dbc8d75275ca64abb40850
serjmac <serjmac@gmail.com>
2021-01-18 16:50:15 +0000

    Init

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
+
+app.get('/', (req,res)=>{
+	res.render('home')
+})
+
+app.listen(port, ()=>{
+console.log(`listening on port ${port}`)
+})
